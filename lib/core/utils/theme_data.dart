cad00e8736dbf7fd16c4e924293e163153c270df
Belal Eid <91228516+belaleid5@users.noreply.github.com>
2025-05-12 15:05:06 -0700

    commet "theme date"

---
+++ lib/core/utils/theme_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+ThemeData themeData() {
+    return ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              scrolledUnderElevation: 0.0,
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.white,
+              elevation: 0,
+            ),
+            primaryColor: Colors.white,
+          );
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:pharmacy_app/core/helper_functions/material_route.dart';
 import 'package:pharmacy_app/core/services/bloc_observer.dart';
 import 'package:pharmacy_app/core/services/get_it.dart';
 import 'package:pharmacy_app/core/utils/constants.dart';
+import 'package:pharmacy_app/core/utils/theme_data.dart';
 import 'package:pharmacy_app/features/date/manger/date_cubit.dart';
 import 'package:pharmacy_app/features/doctors/presention/manger/doctor_cubit.dart';
 import 'package:pharmacy_app/features/medications/presention/manger/medication_cubit.dart';
@@ -14,7 +15,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
    CustomBlocObserver();
-        await Supabase.initialize(url: urlSubaBase, anonKey: apiKey);
+   await Supabase.initialize(url: urlSubaBase, anonKey: apiKey);
 
   setup();
   runApp(const MyApp());
@@ -31,27 +32,19 @@ class MyApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return MultiBlocProvider(
-          providers: [BlocProvider(create: (context) => 
-          getIt<DoctorsCubit>()..fetchDoctors(),),
-          BlocProvider(create: (context) => 
-          getIt<MedicationsCubit>()..fetchMedications(),
-          ),
-          BlocProvider(create: (context) => 
-          getIt<DatesCubit>()..fetchDates(),
-          ),
+          providers: [
+            BlocProvider(create: (context) => 
+            getIt<DoctorsCubit>()..fetchDoctors(),),
+            BlocProvider(create: (context) => 
+            getIt<MedicationsCubit>()..fetchMedications(),
+            ),
+            BlocProvider(create: (context) => 
+            getIt<DatesCubit>()..fetchDates(),
+            ),
           
           ],
           child: MaterialApp(
-            theme: ThemeData(
-              scaffoldBackgroundColor: Colors.white,
-              appBarTheme: AppBarTheme(
-                scrolledUnderElevation: 0.0,
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.white,
-                elevation: 0,
-              ),
-              primaryColor: Colors.white,
-            ),
+            theme: themeData(),
             debugShowCheckedModeBanner: false,
             initialRoute: SplashView.routName,
             onGenerateRoute: onGenerateRoute,
@@ -60,4 +53,6 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+
+  
 }
